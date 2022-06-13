245d2b4a32618bc2d2260fea455c05da101ab277
LEEGWONSEOK <leestart1023@gmail.com>
2022-06-14 04:10:36 +0900

    [Test] Seed 데이터 image 추가

--- camping_proj/seeds.js
+++ camping_proj/seeds.js
@@ -19,13 +19,15 @@ const seedDB = async () => {
     title: 'First Camp',
     price: 10000, 
     description: 'First description',
+    image: 'https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
     location: '부산시 금정구 남산동',
   });
   const camp2 = new Campground({ 
     title: 'Second Camp',
     price: 15000, 
     description: 'Second description',
-    location: '부산시 해운대구 우1동',
+    image: 'https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    slocation: '부산시 해운대구 우1동',
   });
   await camp1.save();
   await camp2.save();
