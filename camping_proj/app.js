812b7a8804b4a4764dd5c72d625a891d9cf89650
LEEGWONSEOK <leestart1023@gmail.com>
2022-06-17 23:30:46 +0900

    [Feat] Joi 유효성 테스트 추가

--- camping_proj/app.js
+++ camping_proj/app.js
@@ -3,7 +3,9 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+
 const catchAsync = require('./utils/catchAsync');
+const campgroundSchema = require('./utils/validateCampground');
 const ExpressError = require('./utils/ExpressError');
 
 const Campground = require('./models/campground');
@@ -29,6 +31,16 @@ app.get('/', (req, res) => {
   res.render('home');
 })
 
+const validateCampground = (req, res, next) => {  
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map(el => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+}
+
 // All Campgrounds
 app.get('/campgrounds', catchAsync(async (req, res) => {
   const campgrounds = await Campground.find({});
@@ -40,8 +52,8 @@ app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new');
 })
 
-app.post('/campgrounds', catchAsync(async (req, res) => {
-  if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
+  //if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
   const campground = new Campground(req.body.campground);
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`);
@@ -59,7 +71,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async(req, res) => {
   res.render('campgrounds/edit', { campground });
 }))
 
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
   res.redirect(`/campgrounds/${campground._id}`);
@@ -79,8 +91,9 @@ app.all('*', (req, res, next) => {
 
 // Error
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message = 'Error!' } = err;
-  res.status(statusCode).send(message);
+  const { statusCode = 500 } = err;
+  if(!err.message) err.message = 'Error!';
+  res.status(statusCode).render('error', { err });
 })
 
 app.listen(3000, () => {
---
+++ camping_proj/utils/validateCampground.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const Joi = require('joi');
+
+// Schema
+module.exports.campgroundSchema = Joi.object({
+  campground: Joi.object({
+    title: Joi.string().required(),
+    price: Joi.number().required().min(0),
+    image: Joi.string().required(),
+    location: Joi.string().required(),
+    description: Joi.string().required()
+  }).required()
+})  
+
+// validate
