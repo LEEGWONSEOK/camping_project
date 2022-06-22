6f37481368d071202bce67539655ae3c1bc9aa81
LEEGWONSEOK <leestart1023@gmail.com>
2022-06-22 20:14:08 +0900

    Refactor: 변수명 변경

--- camping_proj/routes/campgrounds.js
+++ camping_proj/routes/campgrounds.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const { isLoggedIn, validateCampground, isAuthor } = require('./middleware/middleware');
+const { isLoggedIn, validateCamp, isAuthor } = require('./middleware/middleware');
 const campCtrl = require('../controllers/campgrounds');
 
 // All Campgrounds
-router.get('', catchAsync(campCtrl.readAllCamp))
+router.get('/', catchAsync(campCtrl.readAllCamp))
 
 // Create Campground
 router.get('/new', isLoggedIn, campCtrl.readNewCamp)
-router.post('/', isLoggedIn, validateCampground, catchAsync(campCtrl.createCamp))
+router.post('/', isLoggedIn, validateCamp, catchAsync(campCtrl.createCamp))
 
 // Show Campground
 router.get('/:id', catchAsync(campCtrl.readCamp))
 
 // Update Campground
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campCtrl.readCampEdit))
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campCtrl.updateCamp))
+router.put('/:id', isLoggedIn, isAuthor, validateCamp, catchAsync(campCtrl.updateCamp))
 
 // Delete Campground
 router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campCtrl.deleteCamp))
