4d53fcfef29be67d9deb128fb7bf4f84bfa89094
Lee Gwon Seok <leestart1023@gmail.com>
2022-08-15 23:52:29 +0900

    경로 수정

--- camping_proj/routes/middleware/middleware.js
+++ camping_proj/routes/middleware/middleware.js
@@ -4,7 +4,6 @@ const Campground = require('../../models/campground');
 const Review = require('../../models/review');
 
 module.exports.isLoggedIn = (req, res, next) => {
-  //console.log('req.user..', req.user);
   if (!req.isAuthenticated()) {
     req.session.returnTo = req.originalUrl;
     req.flash('error', '로그인 이후 가능합니다.');
