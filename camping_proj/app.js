ca1f1ed5bd6cade738090765f178ef16f18ec702
Lee Gwon Seok <leestart1023@gmail.com>
2022-08-16 01:39:59 +0900

    아틀라스 TEST

--- camping_proj/app.js
+++ camping_proj/app.js
@@ -21,9 +21,9 @@ dotenv.config();
 //   dotenv.config();
 // }
 
-
 // MongoDB app Connect 
-mongoose.connect('mongodb://localhost:27017/camping_proj');
+//mongoose.connect('mongodb://localhost:27017/camping_proj');
+mongoose.connect(process.env.MONGODB_ATLAS_URL);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, "connection error:"));
 db.once('open', () => console.log("✅ MongoDB Connected"));
